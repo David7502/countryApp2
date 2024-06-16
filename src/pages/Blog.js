6e06ed9575b458bd728ed28a8b33e9edd65f819f
David7502 <davidguillo87@gmail.com>
2024-06-16 15:25:02 +0200

    Commit with Blog section added

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
+import axios from "axios";
+import Article from "../components/Article";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+
+  useEffect(() => getData(), []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (content.length < 140) {
       setError(true);
+    } else {
+      setError(false);
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+        //In JS, author is equivalent to author: author
+      });
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
 
@@ -20,14 +44,28 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Name" />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
           onChangeCapture={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
+        {error && <p>Please write a minimum of 140 caracters</p>}
         <input type="submit" value="Send" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
